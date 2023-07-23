15dcc66cce8071addcffa01cc735cc7de16e8cc6
Shibodd <simonebondi456@gmail.com>
2023-07-23 08:28:09 +0200

    Zcp

--- cobs.c
+++ cobs.c
@@ -18,49 +18,58 @@ MmrCobsResult MMR_COBS_BeginEncode(RingBuffer* dst, int* dst_idx, int* cobs_idx)
   return MMR_COBS_OK;
 }
 
-MmrCobsResult MMR_COBS_ContinueEncode(const uint8_t* src, int src_len, RingBuffer* dst, int* dst_idx, int* cobs_idx) {
+MmrCobsResult MMR_COBS_ContinueEncode(const uint8_t* src, int src_len, RingBuffer* dst, int* p_dst_idx, int* p_cobs_idx) {
   if (src_len <= 0) {
     return MMR_COBS_INVALID_SOURCE_LENGTH;
   }
 
+  int base_idx = dst->count;
+  int dst_idx = *p_dst_idx;
+  int cobs_idx = *p_cobs_idx;
+
   int src_idx = 0;
 
   while (src_idx < src_len) {
-    if (*dst_idx >= dst->len - dst->count) {
+    if (dst_idx >= dst->len - base_idx) {
       return MMR_COBS_NO_DST_SPACE_AVAILABLE;
     }
 
     uint8_t byte = src[src_idx];
 
-    int dist_from_cobs = *dst_idx - *cobs_idx;
+    int dist_from_cobs = dst_idx - cobs_idx;
     if (dist_from_cobs >= UINT8_MAX) {
-      *ring_buf_ref(dst, dst->count + *cobs_idx) = UINT8_MAX;
-      *cobs_idx = *dst_idx;
+      *ring_buf_ref(dst, base_idx + cobs_idx) = UINT8_MAX;
+      cobs_idx = dst_idx;
 
       // Advance dst without consuming any input
-      ++(*dst_idx);
+      ++dst_idx;
     } else {
       if (byte == 0) {
-        *ring_buf_ref(dst, dst->count + *cobs_idx) = dist_from_cobs;
-        *cobs_idx = *dst_idx;
+        *ring_buf_ref(dst, base_idx + cobs_idx) = dist_from_cobs;
+        cobs_idx = dst_idx;
       } else {
-        *ring_buf_ref(dst, dst->count + *dst_idx) = byte;
+        *ring_buf_ref(dst, base_idx + dst_idx) = byte;
       }
 
       ++src_idx;
-      ++(*dst_idx);
+      ++dst_idx;
     }
   }
+
+  // Commit
+  *p_dst_idx = dst_idx;
+  *p_cobs_idx = cobs_idx;
   return MMR_COBS_OK;
 }
 
 MmrCobsResult MMR_COBS_EndEncode(RingBuffer* dst, int* dst_idx, int* cobs_idx) {
-  if (*dst_idx >= dst->len - dst->count) {
+  int base_idx = dst->count;
+  if (*dst_idx >= dst->len - base_idx) {
     return MMR_COBS_NO_DST_SPACE_AVAILABLE;
   }
 
-  *ring_buf_ref(dst, dst->count + *cobs_idx) = *dst_idx - *cobs_idx;
-  *ring_buf_ref(dst, dst->count + *dst_idx) = 0;
+  *ring_buf_ref(dst, base_idx + *cobs_idx) = *dst_idx - *cobs_idx;
+  *ring_buf_ref(dst, base_idx + *dst_idx) = 0;
   dst->count += *dst_idx + 1;
   return MMR_COBS_OK;
 }
---
+++ zcp.h
@@ -0,0 +1,68 @@
+
+
+#include <ring_buf.h>
+#include <stdint.h>
+
+typedef bool (*MmrZcpBeginTransmission)(uint8_t* data, int size);
+typedef bool (*MmrZcpTransmissionCompleted)();
+typedef void (*MmrZcpResetCrc)();
+typedef void (*MmrZcpAccumulateCrc)(uint8_t* data, int size);
+typedef uint16_t (*MmrZcpGetCrc)();
+
+
+/*
+ZCP: Zero-termination and Checksum Protocol
+
+Frame format:
+DATA  [variable]
+CRC16 [2]
+ZERO  [1]
+
+*/
+typedef struct MmrZcpInstance {
+  // The buffer holding the data waiting for transmission.
+  RingBuffer txBuf;
+
+  // Whether a write-to-txBuf transaction is in progress.
+  bool txTransactionInProgress;
+
+  // The size of the write-to-txBuf transaction.
+  int txTransactionSize;
+
+  // Temporary indices for COBS encoding during a transaction.
+  int txTransactionDstIdx;
+
+  // Temporary indices for COBS encoding during a transaction.
+  int txTransactionCobsIdx;
+
+  // Whether a transmission started with txBegin is in progress.
+  bool txTransmitting;
+
+  // The size of the data which is being transmitted.
+  int txTransmissionSize;
+
+  // Start transmitting data. If this returns false, transmission will be re-attempted at the next iteration.
+  MmrZcpBeginTransmission txBegin;
+
+  // Whether the transmission started by the last call to txBegin is completed (and thus the data can be forgotten).
+  MmrZcpTransmissionCompleted txCmplt;
+
+  MmrZcpResetCrc crcReset;
+  MmrZcpGetCrc crcGet;
+  MmrZcpAccumulateCrc crcAccumulate;
+} MmrZcpInstance;
+
+void MMR_ZCP_Init(
+    MmrZcpInstance* instance,
+    uint8_t* txbuf,
+    int txbuf_size,
+    MmrZcpBeginTransmission txBegin,
+    MmrZcpTransmissionCompleted txCmplt,
+    MmrZcpResetCrc crcReset,
+    MmrZcpGetCrc crcGet,
+    MmrZcpAccumulateCrc crcAccumulate);
+void MMR_ZCP_Run(MmrZcpInstance* instance);
+bool MMR_ZCP_TxTransactionBegin(MmrZcpInstance* instance);
+bool MMR_ZCP_TxTransactionAppend(MmrZcpInstance* instance, uint8_t* data, int len);
+bool MMR_ZCP_TxTransactionCommit(MmrZcpInstance* instance);
+void MMR_ZCP_TxTransactionAbort(MmrZcpInstance* instance);
