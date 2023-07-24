0e124b1fcde46677aff10c5f26a70bdb320ec455
Shibodd <simonebondi456@gmail.com>
2023-07-24 14:12:57 +0200

    Optimizing for binary size

--- back_logger.c
+++ back_logger.c
@@ -10,12 +10,31 @@ static inline uint32_t MMR_GetTick() {
   return uwTick;
 }
 
+
+
+#define WRITE_BEGIN(id) writeBegin(id)
+#define WRITE_ASSERT(x) if (!(x)) { MMR_ZCP_TxTransactionAbort(HZCP); return false; }
+#define WRITE_APPEND(x) MMR_ZCP_TxTransactionAppend(HZCP, little_endian(x), sizeof(x))
+#define WRITE_END() writeEnd()
 #define little_endian(x) ((uint8_t*)(&(x)))
 #define HZCP (&loggerZcpInstance)
-#define WRITE_ASSERT(x) if (!(x)) { MMR_ZCP_TxTransactionAbort(HZCP); return false; }
-#define WRITE_BEGIN() MMR_ZCP_TxTransactionBegin(HZCP)
-#define WRITE_APPEND(x) MMR_ZCP_TxTransactionAppend(HZCP, little_endian(x), sizeof(x))
-#define WRITE_END() MMR_ZCP_TxTransactionCommit(HZCP)
+
+static bool writeBegin(uint16_t msg_id) {
+  uint32_t tick = MMR_GetTick();
+
+  return MMR_ZCP_TxTransactionBegin(HZCP)
+      && MMR_ZCP_TxTransactionAppend(HZCP, little_endian(seq_id), sizeof(seq_id))
+      && MMR_ZCP_TxTransactionAppend(HZCP, little_endian(tick), sizeof(tick))
+      && MMR_ZCP_TxTransactionAppend(HZCP, little_endian(msg_id), sizeof(msg_id));
+}
+static bool writeEnd() {
+  if (MMR_ZCP_TxTransactionCommit(HZCP)) {
+    ++seq_id;
+    return true;
+  }
+  return false;
+}
+
 #include "log_gen_defs.h"
 
 void MMR_BACK_LOGGER_Init(MmrZcpBsp zcpBsp) {
